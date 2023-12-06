7bc87043c78587e0aab4e9265d841d5bd86f9b51
yoda31217 <jbourne.nik@gmail.com>
2023-12-06 14:50:58 +0200

    2023/06 solution - optimized

--- src/2023/06/aoc-2023-06-part-2-solution.ts
+++ src/2023/06/aoc-2023-06-part-2-solution.ts
@@ -3,7 +3,7 @@ export function calc(input: string) {
     .trim()
     .split('\n')
     .map((line) => line.split(/:/)[1])
-    .map((line) => line.replace(/\s+/g, ''))
+    .map((numberRawStr) => numberRawStr.replace(/\s+/g, ''))
     .map(Number);
 
   let waysToWinCount: number = 0;
