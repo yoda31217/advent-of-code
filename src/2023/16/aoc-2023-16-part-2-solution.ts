1d567c86f06c7a4b271f4744ac2666b09907be7a
yoda31217 <jbourne.nik@gmail.com>
2023-12-17 22:39:50 +0200

    2023 Day 16 solution - refactoring

--- src/2023/16/aoc-2023-16-part-2-solution.ts
+++ src/2023/16/aoc-2023-16-part-2-solution.ts
@@ -1,110 +1,103 @@
-import { add, max } from 'lodash';
+import { add } from 'lodash';
 import { initArray } from '../../utils/arrays';
 import { splitGrid } from '../../utils/strings';
 
 export function calc(input: string) {
   let grid = splitGrid(input);
 
-  // calc2(grid, [
-  //   [0, 0],
-  //   [0, 1],
-  // ]);
-
-  let r = [];
-  for (let i = 0; i < grid.length; i++) {
-    r.push(
-      calc2(grid, [
-        [i, 0],
+  let maxEnergizedTilesCount = 0;
+  for (let row = 0; row < grid.length; row++) {
+    maxEnergizedTilesCount = Math.max(
+      maxEnergizedTilesCount,
+      calculateEnergizedTilesCount(grid, [
+        [row, 0],
         [0, 1],
       ]),
     );
-    r.push(
-      calc2(grid, [
-        [i, grid[0].length - 1],
+    maxEnergizedTilesCount = Math.max(
+      maxEnergizedTilesCount,
+      calculateEnergizedTilesCount(grid, [
+        [row, grid[0].length - 1],
         [0, -1],
       ]),
     );
   }
 
   for (let i = 0; i < grid[0].length; i++) {
-    r.push(
-      calc2(grid, [
+    maxEnergizedTilesCount = Math.max(
+      maxEnergizedTilesCount,
+      calculateEnergizedTilesCount(grid, [
         [0, i],
         [1, 0],
       ]),
     );
-    r.push(
-      calc2(grid, [
+    maxEnergizedTilesCount = Math.max(
+      maxEnergizedTilesCount,
+      calculateEnergizedTilesCount(grid, [
         [grid.length - 1, i],
         [-1, 0],
       ]),
     );
   }
 
-  return max(r);
-  // console.log(grid);
+  return maxEnergizedTilesCount;
 }
 
-export function calc2(grid: string[][], startBeam: [[number, number], [number, number]]) {
-  let energized = initArray(grid.length, () => initArray(grid[0].length, () => false));
-  // console.log(energized);
+export function calculateEnergizedTilesCount(grid: string[][], startBeam: [[number, number], [number, number]]) {
+  let rowToColumnToIsTileEnergized = initArray(grid.length, () => initArray(grid[0].length, () => false));
+  let beamStateKeyToWasProcessed = {};
 
-  let beams = [];
-  beams.push(startBeam);
-
-  let i = 0;
-
-  let was = {};
+  let beams = [startBeam];
 
   while (beams.length > 0) {
-    let [[y, x], [dy, dx]] = beams.pop();
+    let [[row, col], [dRow, dCol]] = beams.pop();
 
-    while (y >= 0 && y < grid.length && x >= 0 && x < grid[0].length) {
-      let key = x + '=' + y + '=' + dx + '=' + dy;
-      if (was[key]) {
+    while (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
+      let beamStateKey = col + '=' + row + '=' + dCol + '=' + dRow;
+      if (beamStateKeyToWasProcessed[beamStateKey]) {
         break;
       }
 
-      was[key] = true;
+      beamStateKeyToWasProcessed[beamStateKey] = true;
 
-      let char = grid[y][x];
+      let char = grid[row][col];
 
-      energized[y][x] = true;
+      rowToColumnToIsTileEnergized[row][col] = true;
 
-      if (char === '-' && dy !== 0) {
+      if (char === '-' && dRow !== 0) {
         beams.push(
           [
-            [y, x - 1],
+            [row, col - 1],
             [0, -1],
           ],
           [
-            [y, x + 1],
+            [row, col + 1],
             [0, 1],
           ],
         );
         break;
-      } else if (char === '|' && dx !== 0) {
+      } else if (char === '|' && dCol !== 0) {
         beams.push(
           [
-            [y - 1, x],
+            [row - 1, col],
             [-1, 0],
           ],
           [
-            [y + 1, x],
+            [row + 1, col],
             [1, 0],
           ],
         );
         break;
       } else if (char === '\\') {
-        [dx, dy] = [dy, dx];
+        [dCol, dRow] = [dRow, dCol];
       } else if (char === '/') {
-        [dx, dy] = [-dy, -dx];
+        [dCol, dRow] = [-dRow, -dCol];
       }
 
-      x += dx;
-      y += dy;
+      col += dCol;
+      row += dRow;
     }
   }
 
-  return energized.map((s) => s.filter((i) => i).length).reduce(add, 0);
+  return rowToColumnToIsTileEnergized.map((s) => s.filter((i) => i).length).reduce(add, 0);
 }
